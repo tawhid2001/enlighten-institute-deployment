a8e4e8b607946e53c26602bc98d690ac34085725
tawhid2001 <tawhid.talal@gmail.com>
2024-10-19 11:07:14 -0400

    first commit

--- Enlighten/settings.py
+++ Enlighten/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import environ
 env = environ.Env()
 environ.Env.read_env()
+EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
