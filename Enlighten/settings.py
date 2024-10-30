bcf0d00fc8bc5a0e57fdffb9dfcfece67dd17d73
tawhid2001 <tawhid.talal@gmail.com>
2024-10-30 12:12:40 -0400

    trusted origin added stripe

--- Enlighten/settings.py
+++ Enlighten/settings.py
@@ -99,6 +99,7 @@ AUTHENTICATION_BACKENDS = (
 CORS_ALLOW_ALL_ORIGINS = True
 
 CSRF_TRUSTED_ORIGINS = [
+    "https://stripe.com",
     "http://127.0.0.1:8000",
     'http://127.0.0.1:5500',
     'http://localhost:5500',
