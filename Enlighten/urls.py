1f82bd228659885eaf146115f74639203dfbe6d5
tawhid2001 <tawhid.talal@gmail.com>
2024-10-30 11:39:26 -0400

    success url updated

--- Enlighten/urls.py
+++ Enlighten/urls.py
@@ -36,5 +36,5 @@ urlpatterns = [
     path('create-checkout-session/', create_checkout_session, name='create-checkout-session'),
     path('stripe_webhook', stripe_webhook, name='stripe-webhook'),
     path('get-publishable-key/', get_publishable_key, name='get-publishable-key'), 
-    path('success/', success_view, name='success-view'), 
+    path('success/', success_view, name='success'), 
 ]
